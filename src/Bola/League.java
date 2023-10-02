8b3433f138e18315b6d3448f387da5af6675d4c9
irfanindrayana <irfankusumaindrayana@gmail.com>
2023-10-02 19:03:13 +0700

    first commit

---
+++ src/Bola/League.java
@@ -0,0 +1,9 @@
+package Bola;
+
+public class League {
+	public String leagueName;
+	public byte season;
+	public String teamsList;
+	public String matchSchedule;
+	public int ranking;
+}
---
+++ src/Bola/Referee.java
@@ -0,0 +1,18 @@
+package Bola;
+/*
+R Mochamad Irfan Kusuma Indrayana
+1202220316
+SI4604
+*/
+public class Referee {
+	public String refereeName;
+	public String refereeLicence;
+	
+    public void printreferee() {
+    	System.out.println("Nama Wasit 	: " + refereeName);
+    	System.out.println("Lisensi 	: " + refereeLicence);
+
+    	System.out.println("======================");
+    }
+
+}
